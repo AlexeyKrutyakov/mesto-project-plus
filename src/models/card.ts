1062a8a5cccd6f49084ecfd32a036bfe7c13df91
Alexey Krutyakov <coder.arctik@gmail.com>
2024-02-23 21:43:57 +0300

    feat: add card model for db

---
+++ src/models/card.ts
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+
+type Card = {
+  name: string;
+  link: string;
+  owner: mongoose.ObjectId;
+  likes: mongoose.ObjectId[];
+  createdAt: Date;
+};
+
+const cardSchema = new mongoose.Schema<Card>({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+  },
+  likes: [
+    {
+      type: mongoose.Types.ObjectId,
+      default: [],
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+export default mongoose.model('card', cardSchema);
