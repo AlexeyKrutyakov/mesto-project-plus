0f8ece2a222a4b46caa60b4b68fd8713a999d174
Alexey Krutyakov <coder.arctik@gmail.com>
2024-02-24 23:15:05 +0300

    refactor: app

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,28 @@
-import express from 'express';
+import 'dotenv/config';
+import express, { Router, json } from 'express';
 import mongoose from 'mongoose';
+import userRouter from './user/user.router';
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3000, MONGO_URL = '' } = process.env;
 
 const app = express();
+const router = Router();
+router.use('/users', userRouter);
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+app.use(json());
+app.use(router);
 
-app.listen(PORT, () => {});
+const connect = async () => {
+  try {
+    mongoose.set('strictQuery', true);
+    await mongoose.connect(MONGO_URL);
+    console.log(`connected with ${MONGO_URL}`);
+
+    await app.listen(PORT);
+    console.log(`server run on port ${PORT}`);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+connect();
