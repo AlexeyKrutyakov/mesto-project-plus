e756fb5a2c00c989757513244976074272a2bfba
Alexey Krutyakov <coder.arctik@gmail.com>
2024-02-25 18:41:10 +0300

    feat: add deleteCardById to card.controllers

--- src/card/card.controllers.ts
+++ src/card/card.controllers.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { constants } from 'http2';
+import { Error as MongooseError } from 'mongoose';
 import errorText from '../constants/errors';
 import Card from './card.model';
 
@@ -20,5 +21,29 @@ export const createCard = async (req: Request, res: Response) => {
     return res.status(constants.HTTP_STATUS_CREATED).send(await newCard.save());
   } catch (error) {
     return res.send(error);
+  }
+};
+
+export const deleteCardById = async (req: Request, res: Response) => {
+  try {
+    const { cardId } = req.params;
+    await Card.findByIdAndDelete(cardId).orFail(() => {
+      const error = new Error('card not found');
+      error.name = 'NotFoundError';
+      return error;
+    });
+    return res.send({ message: 'card deleted' });
+  } catch (error) {
+    if (error instanceof MongooseError.CastError) {
+      return res
+        .status(constants.HTTP_STATUS_BAD_REQUEST)
+        .send({ message: 'bad request, not valide cardId' });
+    }
+    if (error instanceof Error && error.name === 'NotFoundError') {
+      return res
+        .status(constants.HTTP_STATUS_NOT_FOUND)
+        .send({ message: error.message });
+    }
+    return res.send({ error });
   }
 };
--- src/card/card.router.ts
+++ src/card/card.router.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { createCard, getCards } from './card.controllers';
+import { createCard, deleteCardById, getCards } from './card.controllers';
 
 const cardRouter = Router();
 
 cardRouter.get('/', getCards);
 cardRouter.post('/', createCard);
+cardRouter.delete('/:cardId', deleteCardById);
 
 export default cardRouter;
