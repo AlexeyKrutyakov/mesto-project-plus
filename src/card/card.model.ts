ebaaf7c67670967afd770fbc9cca4f3a198b65a0
Alexey Krutyakov <coder.arctik@gmail.com>
2024-02-25 19:34:52 +0300

    fix: remove versionKey from card model

--- src/card/card.model.ts
+++ src/card/card.model.ts
@@ -1,31 +1,36 @@
 import mongoose from 'mongoose';
 import { Card } from './card.type';
 
-const cardSchema = new mongoose.Schema<Card>({
-  name: {
-    type: String,
-    required: [true, 'Поле обязательно для заполнения'],
-    minlength: [2, 'Минимальная длина 2 символа'],
-    maxlength: [30, 'Максимальная длина 30 символов'],
-  },
-  link: {
-    type: String,
-    required: [true, 'Поле обязательно для заполнения'],
-  },
-  owner: {
-    type: mongoose.Types.ObjectId,
-    required: [true, 'Поле обязательно для заполнения'],
-  },
-  likes: [
-    {
-      type: mongoose.Types.ObjectId,
-      default: [],
+const cardSchema = new mongoose.Schema<Card>(
+  {
+    name: {
+      type: String,
+      required: [true, 'Поле обязательно для заполнения'],
+      minlength: [2, 'Минимальная длина 2 символа'],
+      maxlength: [30, 'Максимальная длина 30 символов'],
     },
-  ],
-  createdAt: {
-    type: Date,
-    default: Date.now(),
+    link: {
+      type: String,
+      required: [true, 'Поле обязательно для заполнения'],
+    },
+    owner: {
+      type: mongoose.Types.ObjectId,
+      required: [true, 'Поле обязательно для заполнения'],
+    },
+    likes: [
+      {
+        type: mongoose.Types.ObjectId,
+        default: [],
+      },
+    ],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
   },
-});
+  {
+    versionKey: false,
+  },
+);
 
 export default mongoose.model('card', cardSchema);
