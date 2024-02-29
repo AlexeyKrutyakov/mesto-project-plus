98b9228a520f4bf3f3e96ac80cbd9a99ede5b5c5
Alexey Krutyakov <coder.arctik@gmail.com>
2024-02-29 22:03:18 +0300

    feat: add errorText constants

--- src/constants/errors.ts
+++ src/constants/errors.ts
@@ -1,5 +1,13 @@
 const errorText = {
-  INTERNAL_SERVER_ERROR: 'internal server error',
+  REQUEST_BODY_IS_NOT_VALID: 'Data in request body is not valid, check it',
+  CELEBRATE_VALIDATION_FAILED: 'Validation failed',
+  NOT_VALID_USER_ID: 'Not valid user id',
+  USER_NOT_FOUND: 'User not found',
+  NOT_VALID_CARD_ID: 'Not valid card id',
+  CARD_NOT_FOUND: 'Card not found',
+  BAD_REQUEST_ERROR: 'Bad Request Error',
+  NOT_FOUND_ERROR: 'Not Found Error',
+  INTERNAL_SERVER_ERROR: 'Internal Server Error',
 };
 
 export default errorText;
