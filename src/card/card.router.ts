c67258c1d3f6650b0a542b5ea89d7278fd1ecfdf
Alexey Krutyakov <coder.arctik@gmail.com>
2024-02-29 19:08:30 +0300

    feat: add verifying for post card request

--- src/card/card.router.ts
+++ src/card/card.router.ts
@@ -1,10 +1,22 @@
 import { Router } from 'express';
+import { celebrate, Joi } from 'celebrate';
 import { createCard, deleteCardById, getCards } from './card.controllers';
 
 const cardRouter = Router();
 
 cardRouter.get('/', getCards);
-cardRouter.post('/', createCard);
+cardRouter.post(
+  '/',
+  celebrate({
+    body: Joi.object()
+      .keys({
+        name: Joi.string().required().min(2).max(30),
+        link: Joi.string().required(),
+      })
+      .unknown(true),
+  }),
+  createCard,
+);
 cardRouter.delete('/:cardId', deleteCardById);
 
 export default cardRouter;
