8725b60d6678bf9233a6dd7d700d6ad5b3582326
Alexey Krutyakov <coder.arctik@gmail.com>
2024-02-29 19:06:37 +0300

    feat: add controllers for likes of card

--- src/card/card.controllers.ts
+++ src/card/card.controllers.ts
@@ -56,3 +56,61 @@ export const deleteCardById = async (req: Request, res: Response) => {
       .send({ message: errorText.INTERNAL_SERVER_ERROR });
   }
 };
+
+export const addLikeToCard = async (req: Request, res: Response) => {
+  const { cardId } = req.params;
+  const userId = req.body.owner._id;
+  try {
+    await Card.findByIdAndUpdate(
+      cardId,
+      { $addToSet: { likes: userId } },
+      { new: true },
+    ).orFail(() => {
+      const error = new Error('card not found');
+      error.name = 'NotFoundError';
+      return error;
+    });
+    return res.send(await Card.findById(cardId));
+  } catch (error) {
+    if (error instanceof MongooseError.CastError) {
+      return res
+        .status(constants.HTTP_STATUS_BAD_REQUEST)
+        .send({ message: `not valid cardId ( _id=${cardId} )` });
+    }
+    if (error instanceof Error && error.name === 'NotFoundError') {
+      return res
+        .status(constants.HTTP_STATUS_NOT_FOUND)
+        .send({ message: `card with _id=${cardId} not found` });
+    }
+    return res.send({ error });
+  }
+};
+
+export const removeLikeFromCard = async (req: Request, res: Response) => {
+  const { cardId } = req.params;
+  const userId = req.body.owner._id;
+  try {
+    await Card.findByIdAndUpdate(
+      cardId,
+      { $pull: { likes: userId } },
+      { new: true },
+    ).orFail(() => {
+      const error = new Error('card not found');
+      error.name = 'NotFoundError';
+      return error;
+    });
+    return res.send(await Card.findById(cardId));
+  } catch (error) {
+    if (error instanceof MongooseError.CastError) {
+      return res
+        .status(constants.HTTP_STATUS_BAD_REQUEST)
+        .send({ message: `not valid cardId ( _id=${cardId} )` });
+    }
+    if (error instanceof Error && error.name === 'NotFoundError') {
+      return res
+        .status(constants.HTTP_STATUS_NOT_FOUND)
+        .send({ message: `card with _id=${cardId} not found` });
+    }
+    return res.send({ error });
+  }
+};
