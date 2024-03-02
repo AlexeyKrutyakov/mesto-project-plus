937f191eab5f6d23abeb471b50d30ab2b25b0e47
Alexey Krutyakov <coder.arctik@gmail.com>
2024-03-02 19:15:00 +0300

    feat: add email fields to userSchema

--- src/user/user.model.ts
+++ src/user/user.model.ts
@@ -6,12 +6,13 @@ const userSchema = new mongoose.Schema<User>(
     name: {
       type: String,
       required: [true, 'Поле обязательно для заполнения'],
-      // unique: true // todo is need check for duplicate user ?
       minlength: [2, 'Минимальная длина 2 символа'],
       maxlength: [30, 'Максимальная длина 30 символов'],
     },
     email: {
       type: String,
+      required: [true, 'Поле обязательно для заполнения'],
+      unique: true,
     },
     password: {
       type: String,
