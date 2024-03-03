13ae08dc62f054b942e76d697b32afad2d9f0f35
Alexey Krutyakov <coder.arctik@gmail.com>
2024-03-03 16:47:02 +0300

    feat: add email validation and hash for password to createUser controller

--- src/user/user.controllers.ts
+++ src/user/user.controllers.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
 import { constants } from 'http2';
 import { Error as MongooseError } from 'mongoose';
+import bcrypt from 'bcrypt';
+import validator from 'validator';
 import BadRequestError from '../error/bad-request-error';
 import NotFoundError from '../error/not-found-error';
 import responseMessage from '../constants/responseMessages';
@@ -47,7 +49,16 @@ export const createUser = async (
   next: NextFunction,
 ) => {
   try {
-    const newUser = new User(req.body);
+    const { name, email, password, about, avatar } = req.body;
+    if (!validator.isEmail(email)) {
+      throw new BadRequestError('Not valid email');
+    }
+    const user = await User.find({ email });
+    if (user.length !== 0) {
+      throw new BadRequestError(responseMessage.USER_ALREADY_EXISTS);
+    }
+    const hash = await bcrypt.hash(password, 10);
+    const newUser = new User({ name, email, password: hash, about, avatar });
     // todo ?
     // User.populate(newUser, ['owner, likes'])
     return res.status(constants.HTTP_STATUS_CREATED).send(await newUser.save());
