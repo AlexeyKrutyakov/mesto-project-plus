b9f6be60cb5d586c41702b843f83358d65611aa5
Alexey Krutyakov <coder.arctik@gmail.com>
2024-03-03 16:44:21 +0300

    feat: add elements to responseMessage

--- src/constants/responseMessages.ts
+++ src/constants/responseMessages.ts
@@ -1,5 +1,6 @@
 const responseMessage = {
   CARD_WAS_DELETED: 'Card was deleted',
+  DEFAULT_ERROR: 'Default error',
   REQUEST_BODY_IS_NOT_VALID: 'Data in request body is not valid, check it',
   CELEBRATE_VALIDATION_FAILED: 'Validation failed',
   NOT_VALID_USER_ID: 'Not valid user id',
@@ -9,6 +10,7 @@ const responseMessage = {
   BAD_REQUEST_ERROR: 'Bad Request Error',
   NOT_FOUND_ERROR: 'Not Found Error',
   INTERNAL_SERVER_ERROR: 'Internal Server Error',
+  USER_ALREADY_EXISTS: 'User with the specified email already exists',
 };
 
 export default responseMessage;
