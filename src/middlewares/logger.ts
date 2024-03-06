ee308ee0e8a3af53592643da6124701040d88c50
Alexey Krutyakov <coder.arctik@gmail.com>
2024-03-06 15:07:22 +0300

    feat: add options for logger

--- src/middlewares/logger.ts
+++ src/middlewares/logger.ts
@@ -1,14 +1,16 @@
 import winston from 'winston';
 import expressWinston from 'express-winston';
+import 'winston-daily-rotate-file';
+
+const transport = new winston.transports.DailyRotateFile({
+  filename: 'requests-%DATE%.log',
+  datePattern: 'YYYY-MM-DD-HH',
+  maxSize: '20m',
+  maxFiles: 10,
+  zippedArchive: true,
+});
 
 export default expressWinston.logger({
-  transports: [
-    // new winston.transports.Console({
-    //   format: winston.format.simple(),
-    // }), // console
-    new winston.transports.File({
-      filename: '../request.log',
-    }),
-  ],
+  transports: [transport],
   format: winston.format.json(),
 });
