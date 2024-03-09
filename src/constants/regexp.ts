54381f7b1047239822e281c8a98aaf7c384e27d0
Alexey Krutyakov <coder.arctik@gmail.com>
2024-03-09 20:08:48 +0300

    feat: add mongoObjectId to REGEXP const

--- src/constants/regexp.ts
+++ src/constants/regexp.ts
@@ -2,6 +2,7 @@ const REGEXP = {
   avatar:
     /https?:\/\/(www\.)?(?!www)([a-z0-9-.]*)?[a-z0-9-]*\.[a-z-]{2,}(\/)?([a-z0-9-./_]*)?/gi,
   email: /[a-z0-9-.]{2,}@[a-z0-9-.]*[a-z0-9-]{2,}/gi,
+  mongoObjectId: /^[a-fA-F0-9]{24}$/,
 };
 
 export default REGEXP;
