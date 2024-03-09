511974499b76214456d09d59f6b297cffc34e80b
Alexey Krutyakov <coder.arctik@gmail.com>
2024-03-09 15:16:49 +0300

    fix: returned values from convertCelebrateError

--- src/utils/convertCelebrateError.ts
+++ src/utils/convertCelebrateError.ts
@@ -2,13 +2,20 @@ import { CelebrateError, isCelebrateError } from 'celebrate';
 import { constants } from 'http2';
 import { TError } from '../error/error.type';
 
-export default (error: TError | CelebrateError) => {
+export default (error: TError | CelebrateError): TError => {
   if (error && isCelebrateError(error)) {
-    const celebrateMessage = error.details.get('body')?.details[0].message;
+    // eslint-disable-next-line operator-linebreak
+    const celebrateMessage =
+      error.details.get('body')?.details[0].message || 'unknown error';
     return {
+      name: 'CelebrateError',
       statusCode: constants.HTTP_STATUS_BAD_REQUEST,
       message: celebrateMessage,
     };
   }
-  return { statusCode: error.statusCode, message: error.message };
+  return {
+    name: error.name,
+    statusCode: error.statusCode,
+    message: error.message,
+  };
 };
