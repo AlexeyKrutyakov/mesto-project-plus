3f607b266b4e9343bc37dff6b21bfc1f6845a26e
Alexey Krutyakov <coder.arctik@gmail.com>
2024-03-09 20:07:35 +0300

    feat: add params processing to convertCelebrateError

--- src/utils/convertCelebrateError.ts
+++ src/utils/convertCelebrateError.ts
@@ -1,12 +1,17 @@
 import { CelebrateError, isCelebrateError } from 'celebrate';
 import { constants } from 'http2';
 import { TError } from '../error/error.type';
+import RESPONSE_MESSAGE from '../constants/responseMessages';
 
 export default (error: TError | CelebrateError): TError => {
   if (error && isCelebrateError(error)) {
     // eslint-disable-next-line operator-linebreak
     const celebrateMessage =
-      error.details.get('body')?.details[0].message || 'unknown error';
+      // eslint-disable-next-line operator-linebreak
+      error.details.get('body')?.details[0].message ||
+      // eslint-disable-next-line operator-linebreak
+      error.details.get('params')?.details[0].message ||
+      RESPONSE_MESSAGE.celebrateValidationFailed;
     return {
       name: 'CelebrateError',
       statusCode: constants.HTTP_STATUS_BAD_REQUEST,
